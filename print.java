d6f84bb8233505c48c6718467b4f6e57a25bdf26
ridham02 <ridham1991.be20@chitkara.edu.in>
2021-03-14 17:05:02 +0530

    i have edited file print java in dev branch

--- print.java
+++ print.java
@@ -1,6 +1,9 @@
 public class Git{
 	public static void main(String[] args) {
-		System.out.print("java is fun");
+		System.out.println("java is fun");
+
+		System.out.println("i am in dev branch");
 	}
 }
+
 
